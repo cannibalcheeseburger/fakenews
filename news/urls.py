5353e3aa2299988cca77ab54d998462bbd7d2602
gajendra-surariya <gajendra.surariya0000@gmail.com>
2021-07-11 12:21:43 +0530

    userdetails page added

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,13 @@
-from news.views import HomeListView,NewsDetailView
+from news.views import HomeListView,NewsDetailView,loginView,registerView,logoutUser,UserListView
 from django.urls import path,include
-#AgendaDetailView,
+
 
 urlpatterns = [
     path('',HomeListView.as_view(),name = 'home'),
     path('news_detail/<int:pk>',NewsDetailView.as_view(),name = 'news_detail'),
+    path('login/', loginView, name='login'),
+    path('register/', registerView, name='register'),
+    path('logout/',logoutUser, name='logout'),
+    path('user/', UserListView.as_view(), name='user'),
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import News
 from django.views.generic import ListView,DetailView
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 class HomeListView(ListView):
@@ -13,3 +17,40 @@ class NewsDetailView(DetailView):
     model = News
     template_name = 'news_detail.html'
     context_object_name = 'news'
+
+def loginView(request):
+    context= {}
+    if request.method=="POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request,"Username or Password is incorrect")
+            return render(request, 'login.html', context)
+    return render(request,'login.html',context)
+
+def registerView(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,"Accout Created Successfully")
+            return redirect('login')
+    context= {'form':form}
+    return render(request,'register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+class UserListView(ListView):
+    model = News
+    template_name = 'userdetail.html'
+    context_object_name = 'news'
+    queryset = News.objects.order_by('-date')[:15]
